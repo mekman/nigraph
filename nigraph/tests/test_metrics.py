f49766672d1cc189d8ec20cc6fed5d47c1b19a75
Matthias Ekman <matthias.ekman@gmail.com>
2018-05-22 16:06:09 +0200

    FIX: use deterministic graph for tests

--- nigraph/tests/test_metrics.py
+++ nigraph/tests/test_metrics.py
@@ -12,145 +12,145 @@ def test_degree():
 
 
 def test_betweenness_centrality():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.betweenness_centrality(A, weighted=False)
     npt.assert_equal(A.shape[0], m.shape[0])
 
-    A = nig.get_random_graph(30, weighted=True)
+    A = nig.karate_club(weighted=True)
     m = nig.betweenness_centrality(A, weighted=True)
     npt.assert_equal(A.shape[0], m.shape[0])
 
 
 def test_local_characteristic_path_length():
-    # A = nig.get_random_graph(30, weighted=False)
+    # A = nig.karate_club(weighted=False)
     A = np.zeros((3, 3))
     A[:, 0] = 1
     m = nig.local_characteristic_path_length(A, weighted=False)
     npt.assert_equal(A.shape[0], m.shape[0])
 
-    # A = nig.get_random_graph(30, weighted=True)
+    # A = nig.karate_club(weighted=True)
     m = nig.local_characteristic_path_length(A, weighted=True)
     npt.assert_equal(A.shape[0], m.shape[0])
 
 
 # def test_avg_shortest_path_length():
-#     A = nig.get_random_graph(30, weighted=False)
+#     A = nig.karate_club(weighted=False)
 #     m = nig.avg_shortest_path_length(A, weighted=False)
 #     npt.assert_equal(1, len(m))
 #
-#     A = nig.get_random_graph(30, weighted=True)
+#     A = nig.karate_club(weighted=True)
 #     m = nig.avg_shortest_path_length(A, weighted=True)
 #     npt.assert_equal(1, len(m))
 
 
 def test_nodal_average_shortest_path_length():
-    # A = nig.get_random_graph(30, weighted=False)
-    A = np.zeros((3, 3))
-    A[:, 0] = 1
+    A = nig.karate_club(weighted=False)
+    # A = np.zeros((3, 3))
+    # A[:, 0] = 1
     m = nig.nodal_average_shortest_path_length(A, weighted=False)
     npt.assert_equal(A.shape[0], m.shape[0])
 
-    A = nig.get_random_graph(30, weighted=True)
+    A = nig.karate_club(weighted=True)
     m = nig.nodal_average_shortest_path_length(A, weighted=True)
     npt.assert_equal(A.shape[0], m.shape[0])
 
 
 def test_within_module_degree_z_score():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     n2c, extras = nig.louvain(A, weighted=False, return_tree=False)
     m = nig.within_module_degree_z_score(A, partition=n2c)
     npt.assert_equal(A.shape[0], m.shape[0])
 
 
 def test_participation_coefficient():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     n2c, extras = nig.louvain(A, weighted=False, return_tree=False)
     m = nig.participation_coefficient(A, partition=n2c)
     npt.assert_equal(A.shape[0], m.shape[0])
 
 
 def test_absorption():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.absorption(A)
     npt.assert_equal(A.shape, m.shape)
 
 
 def test_driftness():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.driftness(A)
     npt.assert_equal(A.shape, m.shape)
 
 
 def test_number_k_max():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.number_k_max(A)
     npt.assert_equal(1, np.asarray(m).size)
 
 
 def test_wiring_costs():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.wiring_costs(A)
     npt.assert_equal(1, np.asarray(m).size)
 
 
 def test_efficiency_global():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.efficiency_global(A, weighted=False)
     npt.assert_equal(1, np.asarray(m).size)
 
-    A = nig.get_random_graph(30, weighted=True)
+    A = nig.karate_club(weighted=True)
     m = nig.efficiency_global(A, weighted=True)
     npt.assert_equal(1, np.asarray(m).size)
 
 
 def test_efficiency_local():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.efficiency_local(A, weighted=False)
     npt.assert_equal(1, np.asarray(m).size)
 
-    A = nig.get_random_graph(30, weighted=True)
+    A = nig.karate_club(weighted=True)
     m = nig.efficiency_local(A, weighted=True)
     npt.assert_equal(1, np.asarray(m).size)
 
 
 def test_efficiency_nodal():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.efficiency_nodal(A)
     npt.assert_equal(A.shape[0], m.shape[0])
 
 
 def test_eigenvector_centrality():
-    A = nig.get_random_graph(30)
+    A = nig.karate_club(weighted=False)
     m = nig.eigenvector_centrality(A)
     npt.assert_equal(A.shape[0], m.shape[0])
 
 
 def test_pagerank():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.pagerank(A)
     npt.assert_equal(A.shape[0], m.shape[0])
 
 
 def test_resolvent_centrality():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.resolvent_centrality(A)
     npt.assert_equal(A.shape[0], m.shape[0])
 
 
 def test_subgraph_centrality():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.subgraph_centrality(A)
     npt.assert_equal(A.shape[0], m.shape[0])
 
 
 def test_spread_of_infection():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.spread_of_infection(A)
     npt.assert_equal(A.shape[0], m.shape[0])
 
 
 def test_google_matrix():
-    A = nig.get_random_graph(30, weighted=False)
+    A = nig.karate_club(weighted=False)
     m = nig.google_matrix(A)
     npt.assert_equal(A.shape, m.shape)
 
